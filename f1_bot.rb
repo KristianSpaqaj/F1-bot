4be473b9e40db6a7ede04f6b9f0bcba6bbe58640
KristianSpaqaj <kris49j7@edu.easj.dk>
2022-09-25 18:56:48 +0200

    dont get it

--- f1_bot.rb
+++ f1_bot.rb
@@ -1,22 +1,11 @@
 require 'discordrb'
-token = 'MTAyMzI3Mjk2NzM3Njk5NDQzNw.GaXwn4.vew4jRTNy2YRJYpe_aP9SQWveurWzL6dalgwkg'
-bot = Discordrb::Bot.new token: token
+#Fjern token inden commit
+token = 'lokalt'
 userID = 166555724053217281
+bot = Discordrb::Bot.new token: token
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond('Pong!')
-end
-bot.message(with_text: 'Shutdown!') do |event|
-    shutDown()
-end
-
-def shutDown()
-    bot.command(:exit, help_available: false) do |event|
-        break unless event.user.id == userID
-      
-        bot.send_message(event.channel.id, 'Bot is shutting down')
-        exit
-      end
+bot.message(content: '!ping') do |event|
+  event.respond('Pong')
 end
 
 bot.run
