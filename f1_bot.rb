6e004891aac0e2d1fa2fe6a055914e4cdb3932cb
KristianSpaqaj <kris49j7@edu.easj.dk>
2022-09-28 16:16:55 +0200

    skal nu formater json objekt  så jeg kun viser hvad jeg ville se

--- f1_bot.rb
+++ f1_bot.rb
@@ -1,14 +1,22 @@
 require 'discordrb'
 require './credantials'
-#Fjern token inden commit
+require 'net/http'
+require 'json'
 token = $credantials[:token]
-userID = 166555724053217281
+$link = URI('http://ergast.com/api/f1/')
 bot = Discordrb::Bot.new token: token , client_id: $credantials[:client_id]
 
 bot.message(with_text: 'ping') do |event|
-  puts 'test'
   event.respond 'Pong'
 end
-p bot
+def driver_info(driver)
+  info = $link + "drivers/#{driver}.json"
+  get_info = Net::HTTP.get(info)
+  puts get_info
+  return get_info
+end
+bot.message(with_text: /Get driver (\w+)/) do |event|
+  event.respond driver_info(event.message.content.split[-1])
+end
 bot.run true
 bot.join
